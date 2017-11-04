e775c14e1cedd7d793eef4fcdaaed3e2b2646958
wbaldwin6 <wbaldwin6@gatech.edu>
2017-11-03 20:05:17 -0700

    Moved Log Searching to a child process.
    The log search functionality now runs in a separate thread.

---
+++ logsearch.js
@@ -0,0 +1,59 @@
+var fs = require('fs');
+
+var DomParser = require('dom-parser'); //Used in log searching
+var parser = new DomParser(); //Also used in log searching
+
+var monthenum = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+//This function returns a "Promise", which will "resolve" when it is done
+//When the promise resolves, the searchLogs function will know to send its information to the client
+function searchLogsByFilename(stringToFind, filename) {
+    if(!filename.endsWith('.html')) { //Not a log file
+        return '';
+    } else {
+        var data = fs.readFileSync("./logs/" + filename, 'utf8');
+        if(data){
+            var logfile = parser.parseFromString(data); //Convert the file into a DOM object
+            var bodyOfFile = logfile.getElementsByTagName("body")[0]; //Grab the body of the DOM
+            if(bodyOfFile.textContent.search(stringToFind) !== -1) { //If the string to find is in the body's textContent
+                return filename; //We say we found it
+            } else {
+                return ''; //The empty string works as a "Not Found" value
+            }            
+        }
+    }
+}
+ 
+//This function takes a string and an HTTP Response object
+//Search all the log files, then send the list of good results back to the response
+function searchLogs(stringToFind) {
+    var errorReport = '<body style="background-color:black;color:white;"><b>An error occurred trying to search the logs. Please try again later.</b><br />';
+    fs.readdir("./logs", function(err, files) {
+        if(err) {
+            process.send(errorReport + err + '</body>'); //An error has occured, so tell the user.
+            process.exit();
+        }
+        if(files.length){
+            process.send('<body style="background-color:black;color:white;">');
+            var success = false;
+            for(var i = 0; i < files.length; i++){
+                var result = searchLogsByFilename(stringToFind, files[i]);
+                if(result){
+                    success = true;
+                    process.send('<b><a href="/logs/'+result+'" style="color:blue;">' + result.replace(/([0-9]+)_([0-9]+)_([0-9]+).html/, function(match, p1, p2, p3, offset, string) {
+                        return monthenum[parseInt(p2)-1] + ' ' + p3 + ', ' + p1; //Convert "YYYY_MM_DD.html" into "Monthname DD, YYYY"
+                    }) + '</b><br />');
+                }
+            }
+            if(!success){
+                process.send('<b>No results found!</b>');
+            }
+            process.send('</body>');
+            process.exit();
+        }
+    });
+}
+
+process.on('message', function(m){
+    searchLogs(m);
+});
