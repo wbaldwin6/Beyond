951af8e4ebb3ae04133fd8e2ee6c64dfe5513fe0
wbaldwin6 <wbaldwin6@gatech.edu>
2018-05-27 11:08:20 -0700

    Interactive Logs now have their own buttons.
    Rather than showing the in-log buttons, Interactive Logs now include their own, which will persist even when changing the log page.

--- interactivelogs.js
+++ interactivelogs.js
@@ -7,6 +7,8 @@ var nextoff = false;
 var url = document.URL.split('/');
 var room = url.length == 6 ? url[url.length-2]+'/' : '';
 
+var io = [1,1];
+
 function refresh(){
     document.getElementById('fileselect').selectedIndex = currententry;
     document.getElementById('ifrm').setAttribute("src", '/logs/'+room+logfilelist[currententry]);
@@ -47,6 +49,39 @@ function refresh(){
     }
 }
 
+function onifload(){
+    var buttons = ifrm.contentDocument.getElementsByClassName('buttons');
+    if(buttons.length){
+        buttons[0].style.display = "none";
+        var x = ifrm.contentDocument.getElementsByTagName("style")[0].sheet.cssRules;
+        //update this to follow how the buttons are currently set if needed
+        if(tsbutton.innerHTML.startsWith("Show")){
+            x[5].style.display = "none";
+        }
+        if(delbutton.innerHTML.startsWith("Show")){
+            x[8].style.display = "none";
+        }
+        toggleIO(io[0],io[1]);
+    }
+}
+
+function toggledisplay(e, y){
+    if(e.innerHTML.startsWith("Hide")){
+        e.innerHTML = e.innerHTML.replace("Hide", "Show");
+    } else {
+        e.innerHTML = e.innerHTML.replace("Show", "Hide");
+    }
+    var x = ifrm.contentDocument.getElementsByTagName("style")[0].sheet.cssRules;
+    x[y].style.display = x[y].style.display==="none" ? "initial" : "none";
+}
+
+function toggleIO(i, o){
+    var x = ifrm.contentDocument.getElementsByTagName("style")[0].sheet.cssRules;
+    x[6].style.display = i ? "initial" : "none";
+    x[7].style.display = o ? "initial" : "none";
+    io = [i,o];
+}
+
 //create the 'current file' button
 var currentfilelisting = document.createElement('DIV');
 currentfilelisting.id = 'currentfile';
@@ -80,7 +115,28 @@ ifrm.style.width = 100+"%";
 ifrm.style.borderWidth = '0px';
 ifrm.style.height = 'calc(100% - '+50+'px)';
 ifrm.frameborder = '0';
+ifrm.onload = onifload;
 container.appendChild(ifrm);
+
+//create the Hide Timestamps and Hide Deletes buttons
+var tsbutton = lastbutton.cloneNode(true);
+tsbutton.id = 'ts';
+tsbutton.textContent = 'Hide Timestamps';
+tsbutton.style.fontSize = 'smaller';
+tsbutton.style.width = 'calc(15%)';
+tsbutton.style.whiteSpace = 'nowrap';
+tsbutton.style.height = '20px';
+tsbutton.style.lineHeight = '20px';
+tsbutton.style.borderRadius = '3px';
+tsbutton.style.marginRight = 'calc(2.5%)';
+tsbutton.onclick = function(){toggledisplay(this,5);};
+container.appendChild(tsbutton);
+
+var delbutton = tsbutton.cloneNode(true);
+delbutton.id = 'del';
+delbutton.textContent = 'Hide Deletes';
+delbutton.onclick = function(){toggledisplay(this,8);};
+container.appendChild(delbutton);
 
 //create the 'navigate to next' button
 var nextbutton = lastbutton.cloneNode(true);
@@ -89,5 +145,27 @@ nextbutton.textContent = 'Next Log';
 //nextbutton.setAttribute('style', "display: inline-block; border-radius: 0px 0px 3px 3px; height: 25px; background: #7289DA; color: #fff; line-height: 25px; width: 30%; cursor: pointer");
 nextbutton.onclick = function(){currententry++; refresh();};
 container.appendChild(nextbutton);
+
+var OOCbutton = tsbutton.cloneNode(true);
+OOCbutton.id = "OOCb";
+OOCbutton.textContent = 'OOC Only';
+OOCbutton.style.width = 'calc(10%)';
+OOCbutton.style.marginLeft = 'calc(2%)';
+OOCbutton.style.marginRight = '0';
+OOCbutton.onclick = function(){toggleIO(0,1);};
+container.appendChild(OOCbutton);
+
+var ICbutton = OOCbutton.cloneNode(true);
+ICbutton.id = "ICb";
+ICbutton.textContent = 'IC Only';
+ICbutton.style.marginLeft = 'calc(1.5%)';
+ICbutton.onclick = function(){toggleIO(1,0);};
+container.appendChild(ICbutton);
+
+var bothbutton = ICbutton.cloneNode(true);
+bothbutton.id = "bothb";
+bothbutton.textContent = 'Both';
+bothbutton.onclick = function(){toggleIO(1,1);};
+container.appendChild(bothbutton);
 
 refresh();
